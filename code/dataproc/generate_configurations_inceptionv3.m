85902a1f8baf585e29d86d942410071081e5b6cd
Cody Shearer <cjshearer@live.com>
2020-10-01 12:27:20 -0400

    extracted one-hot-encoded configurations from inceptionv3 data

---
+++ code/dataproc/generate_configurations_inceptionv3.m
@@ -0,0 +1,12 @@
+function [configurations] = generate_configurations_inceptionv3(fileIn)
+%PREPROC_INCEPTIONV3 extract configurations from inceptionv3 data
+%   Given a file for inceptionv3 data yeilds a table of one-hot-encoded 
+%   configurations.
+table = readtable(fileIn);
+
+configurations = [table(:,7:10) one_hot_enc(table(:,11),'cpu_freq_')       ...cpu status and freq
+                  table(:,12)   one_hot_enc(table(:,13),'gpu_freq_')       ...gpu status and freq
+                  table(:,14)   one_hot_enc(table(:,15),'emc_freq_')       ...emc status and freq
+                  ];
+end
+
